59b10d14acd93b1963624364e8c50a8d3e39e8f8
DanoR <zheung@vip.qq.com>
2018-01-23 15:00:45 +0800

    fix bug 'Topology was destroyed'

--- libs/db.js
+++ libs/db.js
@@ -2,7 +2,14 @@ module.exports = async(dbinfo) => {
 	let connect;
 
 	if(!connect) {
-		connect = await require('mongodb').MongoClient.connect(`mongodb://${dbinfo.user}:${dbinfo.pswd}@${dbinfo.dest}:${dbinfo.port}/${dbinfo.name}`);
+		connect = await require('mongodb').MongoClient.connect(
+			`mongodb://${dbinfo.user}:${dbinfo.pswd}@${dbinfo.dest}:${dbinfo.port}/${dbinfo.name}`, {
+				keepAlive: 300000,
+				connectTimeoutMS: 30000,
+				autoReconnect: true,
+				poolSize: 2
+			}
+		);
 
 		delete dbinfo.user;
 		delete dbinfo.pswd;
