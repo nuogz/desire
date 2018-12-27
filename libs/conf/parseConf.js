e6abdaefb18fcf884104de1645a6e5b67236ccdd
DanoR <zheung@vip.qq.com>
2018-12-27 11:26:20 +0800

    path bug fixed

--- libs/conf/parseConf.js
+++ libs/conf/parseConf.js
@@ -2,11 +2,17 @@ module.exports = async function(confPath, mainConf, cata) {
 	let conf = await require('./loadConf')(confPath, cata);
 
 	if(conf.apps instanceof Array) {
-		mainConf.apps = mainConf.apps.concat(conf.apps);
+		let apps = [];
+
+		for(let path of conf.apps) {
+			apps.push(RC(_pa.parse(confPath).dir, path));
+		}
+
+		mainConf.apps = mainConf.apps.concat(apps);
 	}
 
 	if(conf.path && conf.path.log) {
-		mainConf.path.log = RC(conf.path.log);
+		mainConf.path.log = RC(_pa.parse(confPath).dir, conf.path.log);
 	}
 
 	GG[cata].info(`加载 主[配置]: 路径{${confPath}}`);
