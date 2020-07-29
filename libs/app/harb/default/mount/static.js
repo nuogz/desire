119bf85a77284e48b427ab215afb7de4d1840663
DanoR <zheung@vip.qq.com>
2020-07-29 15:55:49 +0800

    [Harb/Default/Mount/static] check path no more

--- libs/app/harb/default/mount/static.js
+++ libs/app/harb/default/mount/static.js
@@ -3,23 +3,10 @@ module.exports = async function({ G, st }) {
 		let path = static.path;
 
 		try {
-			let stat = _fs.statSync(path);
-
-			if(stat.isDirectory()) {
-				await st(path, static.prefix, static.option);
-			}
-			else {
-				throw Error(`目录路径{[${path}]}非文件夹`);
-			}
+			st(path, static.prefix, static.option);
 		}
 		catch(error) {
-			if(error.code == 'ENOENT') {
-				G.error(`加载 [静态资源]: 失败, 目录路径{[${path}]}不存在`);
-			}
-
-			else {
-				G.error(`加载 [静态资源]: 失败, ${error.message}`);
-			}
+			G.error(`加载 [静态资源]: 失败, ${error.message}`);
 
 			throw error;
 		}
