67ce873ceaab1586d06b384739a92d664db1b860
DanoR <zheung@vip.qq.com>
2020-11-10 15:47:41 +0800

    improve helmet

--- libs/serv/loaderHead.js
+++ libs/serv/loaderHead.js
@@ -4,7 +4,7 @@ const BodyParser = require('koa-bodyparser');
 const Cors = require('@koa/cors');
 const Helmet = require('koa-helmet');
 
-module.exports = async function({ koa }, { cors, http2 }) {
+module.exports = async function({ koa }, { cors }) {
 	// zlib压缩
 	koa.use(Compress({
 		threshold: 2048,
@@ -15,8 +15,30 @@ module.exports = async function({ koa }, { cors, http2 }) {
 	// 请求参数解析
 	koa.use(BodyParser());
 
-	// cors请求头
+	// // cors请求头
 	if(cors) { koa.use(Cors()); }
+
 	// hsts请求头
-	if(http2) { koa.use(Helmet()); }
+	koa.use(Helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: ['\'self\''],
+			objectSrc: ['\'none\''],
+			scriptSrc: ['\'self\'', '\'unsafe-eval\''],
+			scriptSrcAttr: ['\'none\''],
+			styleSrc: ['\'self\'', 'https:', 'unsafe-inline'],
+			imgSrc: ['\'self\'', 'https:', 'data:'],
+			fontSrc: ['\'self\'', 'https:', 'data:'],
+			upgradeInsecureRequests: [],
+			blockAllMixedContent: [],
+		},
+	}));
+	koa.use(Helmet.expectCt());
+	koa.use(Helmet.frameguard());
+	koa.use(Helmet.hidePoweredBy());
+	koa.use(Helmet.hsts());
+	koa.use(Helmet.ieNoOpen());
+	koa.use(Helmet.noSniff());
+	koa.use(Helmet.permittedCrossDomainPolicies());
+	koa.use(Helmet.referrerPolicy());
+	koa.use(Helmet.xssFilter());
 };
