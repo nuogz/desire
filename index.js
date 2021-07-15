348426b929ed9b8402834b283d5ab964001dcb1c
DanoR <zheung@vip.qq.com>
2021-07-16 00:07:03 +0800

    up to `4.10.2`

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import Favicon from 'koa-favicon';
 /**
  * #### 服务器系统（渴望）
  * - 基于`koajs`封装的简单服务器
- * @version 4.10.1-2021.07.15.02
+ * @version 4.10.2-2021.07.16.01
  * @class
  */
 class Desire {
